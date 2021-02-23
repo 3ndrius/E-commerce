b317cd6e2469e761df4938b8e35b852e4d66e8f5
3ndrius <andrzejo1991@gmail.com>
2021-02-23 13:10:47 +0100

    Delete product
    Add delete product controller and route. Add test delete product
    endpoint

--- server/src/controllers/productController.js
+++ server/src/controllers/productController.js
@@ -57,7 +57,6 @@ exports.getSingleProduct = async (req, res, next) => {
 // edit product => /api/v1/product/{id} PUT
 
 exports.updateProduct = async (req, res, next) => {
-  console.log("Body", req.body);
   try {
     let product = await Product.findByIdAndUpdate(req.params.id , req.body)
     res.status(200).json({
@@ -72,4 +71,21 @@ exports.updateProduct = async (req, res, next) => {
     
   }
 
+}
+
+// delete product  => /admin/api/product/{id}  DELETE
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    let product = await Product.findByIdAndDelete(req.params.id)
+    res.status(200).json({
+      success:true,
+      message: "Product deleted successfully",
+    })
+  } catch (error) {
+    res.status(400).json({
+      success:false,
+      error:error.message
+    })
+  }
 }
--- server/src/routes/product.js
+++ server/src/routes/product.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/productController')
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController')
 
 router.route('/products').get(getProducts)
 router.route('/products').post(newProduct)
 router.route('/products/:id').get(getSingleProduct)
 router.route('/admin/products/:id').put(updateProduct)
+router.route('/admin/products/:id').delete(deleteProduct)
 
 module.exports = router
