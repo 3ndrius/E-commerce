7ac980bd12d8b649fd62c110885eab63f95ba16e
3ndrius <andrzejo1991@gmail.com>
2021-02-24 13:19:08 +0100

    Add user model and install dependencies
    Add all user model schema and install jwt, bcryptjs, parser-body/cookie,
    validator

---
+++ server/src/models/user.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+const userSchema = new mongoose.Schema({
+    
+    name: {
+        type: String,
+        required: [true, "Please enter user name"],
+        maxLength: [30, "Your name cannot be more than 30 characters"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please enter your email address"],
+        unique: true,
+        validate:[validator.isEmail, "Please enter a valid email address"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please enter your password"],
+        minLength: [8, "Your password must be at least 8 characters long"],
+        select: false
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    role: {
+        type: String,
+        default: 'user'
+    },
+    createdAt: {
+        type: String,
+        default: date.now
+    },
+    resetPasswordToken: String,
+    resetPasswordExpires: Date
+
+})
+
+module.exports = mongoose.model('User', userSchema )
