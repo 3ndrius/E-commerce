5d245bd8d3ded1bf9cb6abcfc286bdc90b7c62ff
3ndrius <andrzejo1991@gmail.com>
2021-03-02 11:09:05 +0100

    Add route
    Add route, import react lazy

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import "./App.css";
-import Header from "./components/layout/Header";
+import { BrowserRouter, Route } from 'react-router-dom'
 import { ThemeProvider } from "@material-ui/styles";
 import { theme } from "./theme/customTheme";
+import Header from "./components/layout/Header";
 import Home from "./components/Home";
+
 
 function App() {
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
+        <BrowserRouter>
         <Header />
-        <Home />
+        <Route path="/" component={Home} />
+        </BrowserRouter>
       </ThemeProvider>
     </React.Fragment>
   );
