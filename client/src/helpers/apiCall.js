ffa58355a1d6df140543711f8eaf3bc08983c9d4
3ndrius <andrzejo1991@gmail.com>
2021-04-27 22:56:10 +0200

    Clean up

--- client/src/helpers/apiCall.js
+++ client/src/helpers/apiCall.js
@@ -3,9 +3,11 @@ import axios from "axios";
 // Change the baseURL to the servers web address
 // in production
 
-
 export default axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:8080/api/v1/" : `${process.env.REACT_APP_PROD_API_URL}/api/v1/`,
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8080/api/v1/"
+      : `${process.env.REACT_APP_PROD_API_URL}/api/v1/`,
   withCredentials: true,
   credentials: "include",
 });
